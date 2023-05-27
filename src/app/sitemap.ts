7658ee7c0e10ad378f2f7a281f356dce2088be85
Austin Witherow <au.witherow@gmail.com>
2023-05-27 14:59:35 -0400

    update blog post

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 
-export const articles: string[] = ["vampire-facelift-non-surgical-secret-youthful-skin"];
+export const articles: string[] = [
+  "unleashing-the-power-of-the-vampire-facelift-your-non-surgical-key-to-youthful-skin",
+];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
