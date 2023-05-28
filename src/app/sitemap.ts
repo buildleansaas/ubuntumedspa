691195097ff6aad8560d32f0f25f704d178e394a
Austin Witherow <au.witherow@gmail.com>
2023-05-28 16:32:50 -0400

    add joint restoration blog post

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,8 @@ import { MetadataRoute } from "next";
 export const articles: string[] = [
   "unleashing-the-power-of-the-vampire-facelift-your-non-surgical-key-to-youthful-skin",
   "hair-loss-got-you-down-discover-prp-your-new-ally-in-hair-restoration",
+  "beginners-guide-to-platelet-rich-plasma-therapy",
+  "naturally-heal-joint-pain-prp-therapy",
 ];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
