e27f0cc6240642e14e09be25204adb2db6557d8e
Austin Witherow <au.witherow@gmail.com>
2024-07-26 12:53:42 -0400

    add products to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-import { procedures } from "data";
+import { procedures, products } from "data";
 
 export const articles: string[] = [
   "revitalize-sexual-health-female-intimacy-prp-protocols-vaginal-dryness",
@@ -26,6 +26,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     })),
     ...procedures.map((procedure) => ({
       url: `https://www.ubuntumedspa.com/procedures/${procedure.slug}`,
+      lastModified: new Date(),
+    })),
+    ...products.map((product) => ({
+      url: `https://www.ubuntumedspa.com/products/${product.slug}`,
       lastModified: new Date(),
     })),
   ];
